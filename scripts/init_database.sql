1241d75f5b170cf3fff2a392277a4e69e7652857
Paula Chidiac <161069332+paula-chidiac@users.noreply.github.com>
2026-02-11 20:31:02 -0300

    Create init_database.sql
    Scripts de criação do dw e schemas

---
+++ scripts/init_database.sql
@@ -0,0 +1,36 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+
+Objetivo do script: 
+  Garantir que a database datawarehouse seja (re)criada do zero. Adicionalmente, cria os três esquemas para arquitetura medallion.
+
+AVISO:
+  Este script apaga todo o database "datawarehouse", junto com dados armazenados. 
+
+*/
+
+
+--- AVISO: Relizar operação fora da database;
+--- Termina a conexão de todos os usuários do db; 
+
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'datawarehouse';
+
+--- AVISO: Relizar operação fora da database;
+--- Apaga o datawarehouse;
+
+DROP DATABASE IF EXISTS datawarehouse;
+
+--- Cria a database 'datawarehouse'
+
+CREATE DATABASE datawarehouse;
+
+--- AVISO: Realizar operação DENTRO de 'datawarehouse'
+--- Cria os schemas
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA prata;
+CREATE SCHEMA ouro;
